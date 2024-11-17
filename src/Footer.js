bdac0255b2bf704c59d26d2ca420238bec00f967
soodala2003 <soodala78@gmail.com>
2024-11-17 01:15:14 -0500

    Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,19 @@
-import Nav from "./Nav.js";
-
 export default function Footer() {
     return (
         <>
-            <Nav />
-            <h2 style={{ color: "#d3d3d3", fontSize: 20 }}>&copy; 2013 Valet Industries, Inc</h2>
+          <nav aria-label="Main Navigation" role="navigation">
+            <ul className="nav-footer-list">
+                <li><a href="...">Home</a></li>
+                <li><a href="...">Women's</a></li>
+                <li><a href="...">Men's</a></li>
+                <li><a href="...">On the Street</a></li>
+                <li><a href="...">The Catwalk</a></li>
+                <li><a href="...">AdWatch</a></li>
+                <li><a href="...">About</a></li>
+                <li><a href="...">Tips</a></li>
+            </ul>
+          </nav>
+          <h2 style={{ color: "#d3d3d3", fontSize: 20 }}>&copy; 2013 Valet Industries, Inc</h2>
         </>
     );
 }
