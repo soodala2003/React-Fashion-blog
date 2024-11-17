e0066b936274ac357f359cf01f93c252eac34a20
soodala2003 <soodala78@gmail.com>
2024-11-16 23:16:58 -0500

    added Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import Nav from "./Nav.js";
+
+export default function Footer() {
+    return (
+        <>
+            <Nav />
+            <h2 style={{ color: "#d3d3d3", fontSize: 20 }}>&copy; 2013 Valet Industries, Inc</h2>
+        </>
+    );
+}
