6c15870c68d00d3269f7ed11466f7e93acf2844f
soodala2003 <soodala78@gmail.com>
2024-11-16 23:15:52 -0500

    added Header.js

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import Nav from "./Nav.js";
+
+export default function Header() {
+    return (
+        <div>
+            <h1>Sartre's List</h1>
+            <h2>Better-Dressed People</h2>
+            <Nav />
+        </div>
+    );
+}
