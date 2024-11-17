bb3ebc3f99029e5b49c883311597f34a5c61eb3d
soodala2003 <soodala78@gmail.com>
2024-11-16 23:16:25 -0500

    added Nav.js

---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <nav aria-label="Main Navigation" role="navigation">
+            <ul className="nav-list">
+                <li><a href="...">Women's</a></li>
+                <li><a href="...">Men's</a></li>
+                <li><a href="...">On the Street</a></li>
+                <li><a href="...">The Catwalk</a></li>
+                <li><a href="...">AdWatch</a></li>
+                <li><a href="...">About</a></li>
+            </ul>
+        </nav>
+    );
+}
