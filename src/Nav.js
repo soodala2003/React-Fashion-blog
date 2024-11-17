0e43204f40e4b246535b923e45319994f7b955b5
soodala2003 <soodala78@gmail.com>
2024-11-17 01:16:13 -0500

    updated Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,7 @@
-export default function Header() {
+export default function Nav() {
     return (
         <nav aria-label="Main Navigation" role="navigation">
-            <ul className="nav-list">
+            <ul className="nav-header-list">
                 <li><a href="...">Women's</a></li>
                 <li><a href="...">Men's</a></li>
                 <li><a href="...">On the Street</a></li>
